2861af59845483efbfad721d38050fd773c66be3
tanviredu <tanviredu2018@gmail.com>
2023-12-17 17:25:36 +0600

    added

--- 6_API/LittleLemon/LittleLemonAPI/views.py
+++ 6_API/LittleLemon/LittleLemonAPI/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.db import InterfaceError
+from django.http import JsonResponse
+from .models import Book
+from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
 
-# Create your views here.
+
+@csrf_exempt
+def books(request):
+    if request.method == "GET":
+        bks = Book.objects.all().values()
+        return JsonResponse({"books":list(bks)},safe=False)
